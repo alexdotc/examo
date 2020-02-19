cd8209480c95552edd07078cc183ab9be42880b5
np595 <49132942+np595@users.noreply.github.com>
2020-02-18 18:09:21 -0800

    Update Middle490.php

--- alpha/Middle490.php
+++ alpha/Middle490.php
@@ -41,7 +41,7 @@ curl_close($ch);
 
 if($result){ //If true then we want to response back and show the authentication works. Otherwise if the login location also returns then just simply comment out header
         $response = "NJITyes";
-        header("Location: https://myhub.njit.edu/vrs/Step1handler?UCID=adfasadfadfasdf");
+        //header("Location: https://myhub.njit.edu/vrs/Step1handler?UCID=adfasadfadfasdf");
 }
 else{
         $response = "NJITno";
