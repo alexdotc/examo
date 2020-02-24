971d124362942464f2940933864907eec0ce1410
AChavkin <alexlchavkin@gmail.com>
2020-02-24 00:01:34 -0500

    forgot to add file for prev

---
+++ beta/front/ajaxHandler.php
@@ -0,0 +1,38 @@
+<?php
+	define('MAGICNUMBER', true);
+	include 'restrict.php';
+
+        $URL = 'https://web.njit.edu/~np595/CS490Work/middleAlt.php';
+
+	$reqtype = $_POST['RequestType'];
+
+	$post_params = "RequestType=$reqtype";
+
+	switch($reqtype){
+		case 'CreateQuestion':
+			$topic = $_POST['Topic'];
+			$difficulty = $_POST['Difficulty'];
+			$questiontext = $_POST['QuestionText'];
+			$post_params = $post_params . "&Topic=$topic&Difficulty=$difficulty&QuestionText=$questiontext";
+			break;
+		default:
+			break;			
+	}
+
+	$resp = handoff($post_params, $URL);
+	echo $resp;
+
+	function handoff($post_params, $URL){
+		$ch = curl_init();
+		$options = array(CURLOPT_URL => $URL,
+			         CURLOPT_HTTPHEADER =>
+				 array('Content-type:application/x-www-form-urlencoded'),
+				 CURLOPT_RETURNTRANSFER => TRUE,
+				 CURLOPT_POST => TRUE,
+				 CURLOPT_POSTFIELDS => $post_params);
+		curl_setopt_array($ch, $options);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
+	}
+?>
