16c5511cc463bd23b037f4eb9b76e9272cc08bcf
AChavkin <alexlchavkin@gmail.com>
2020-02-24 20:47:49 -0500

    front: ajax to list questions from DB for create exam

--- beta/front/teacherExam.js
+++ beta/front/teacherExam.js
@@ -1,2 +1,23 @@
-console.log("Dynamically inserted script; ACTIVE");
-console.log("Hello.");
+ajaxList(handle);
+
+function ajaxList(callback){
+
+        const SERVER = 'ajaxHandler.php';
+
+        const post_params = "RequestType=GetQuestions";
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", SERVER, true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.onload = function(){
+                if (xhr.status == 200){
+                        let resp = JSON.parse(this.responseText);
+                        callback(resp);
+                }
+        }
+        
+        xhr.send(post_params);
+
+function handle(json){
+        console.log("Received question list");
+}
--- beta/front/teacherExam.php
+++ beta/front/teacherExam.php
@@ -2,4 +2,8 @@
 	define('MAGICNUMBER', true);
 	include 'restrict.php';
 ?>
-<p>Welcome to the teacher "Create Exam" page. More to come soon...</p>
+<div id="ExamMain" class="ExamItems ExamMain">
+    <h2 class="ExamHeader">New Exam</h2>
+    <div id="QuestionList" class="ExamItems ExamQuestions">
+    </div>
+</div>
