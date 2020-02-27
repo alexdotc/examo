97f04fe3f3d2f37ec37a927c7f4e6428927b3b77
AChavkin <alexlchavkin@gmail.com>
2020-02-26 19:02:30 -0500

    front: list questions on exam page

--- beta/front/teacherExam.js
+++ beta/front/teacherExam.js
@@ -19,6 +19,19 @@ function ajaxList(callback){
         xhr.send(post_params);
 }
 
-function handle(json){
-        console.log("Received question list");
+function handle(questions){
+	const divList = document.getElementById("QuestionList");
+
+	for(let question in questions){
+		let li = document.createElement("div");
+
+		li.setAttribute('class', 'QuestionItems QuestionListItems');
+		li.setAttribute('id', 'question');
+		li.innerHTML += '<strong>Topic:</strong> ' + questions[question]['topic'] + '<br />';
+		li.innerHTML += '<strong>Difficulty:</strong> ' + questions[question]['difficulty'] + '<br /><br />';
+		li.innerHTML += questions[question]['questiontext'] + '<br /><br />';
+		li.innerHTML += '<strong>Test Cases:</strong> ' + questions[question]['testcases'] + '<br />';
+		li.innerHTML += '<hr />';
+		divList.appendChild(li);
+	}
 }
