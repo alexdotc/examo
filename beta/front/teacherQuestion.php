2d63e2e4d9bf0a48b9e730a2577b2ffc3778ae0f
AChavkin <alexlchavkin@gmail.com>
2020-02-28 22:47:42 -0500

    front: dropdowns for question + topic

--- beta/front/teacherQuestion.php
+++ beta/front/teacherQuestion.php
@@ -6,9 +6,21 @@
     <h2 class="QuestionHeader">New Question</h2>
     <form id="QuestionForm">
         <label for="Topic" class="QuestionLabel QuestionItems">Topic: </label>
-        <input type="text" name="Topic" id="topic" class="QuestionText QuestionItems" />
+        <select required name="Topic" id="topic" class="QuestionSelect QuestionItems">
+            <option value="Arrays">Arrays</option>
+            <option value="Loops">Loops</option>
+            <option value="Dictionaries">Dictionaries</option>
+            <option value="Generators">Generators</option>
+            <option value="Recursion">Recursion</option>
+            <option value="Lists">Lists</option>
+            <option value="Strings">Strings</option>
+        </select>
         <label for="Difficulty" class="QuestionLabel QuestionItems">Difficulty: </label>
-        <input type="text" name="Difficulty" id="difficulty" class="QuestionText QuestionItems" />
+        <select required name="Difficulty" id="difficulty" class="QuestionSelect QuestionItems">
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
+        </select>
         <label for="VQuestion" class="QuestionLabel QuestionItems">Question: </label>
         <textarea id="VQuestion" form="Question" class="VQuestion QuestionItems" cols="100" rows="10" wrap="soft" placeholder="Enter Your Question Here"></textarea>
         <textarea id="testcase1" form="Question" class="TestCase QuestionItems" cols="100" rows="3" wrap="soft" placeholder="Enter A Test Case Here"></textarea>
