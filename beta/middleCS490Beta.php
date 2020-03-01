0f8f9721f1ecaee5157a2a5e9eaefe1b768e76c8
np595 <49132942+np595@users.noreply.github.com>
2020-03-01 13:30:34 -0800

    Update middleCS490Beta.php

--- beta/middleCS490Beta.php
+++ beta/middleCS490Beta.php
@@ -1,50 +1,27 @@
 <?php
-//Checked with the test and works with sending data from front to back and registers questions and exams, while bringing info of exams
-//and questions back to front.
-$url = 'https://myhub.njit.edu/vrs/ldapAuthenticateServlet';
+
 $backurl = 'https://web.njit.edu/~yav3/backEndCS490Betha.php';
 
 $requestID = $_POST['RequestType'];
 $data = $_POST['data'];
-//Due to no connection of post being sent to back, the back would need the data
-//To call $data['RequestType'] to get the request type
 
 if ($requestID == 'login'){
         $username = $data['ucid'];
         $password = $data['password'];
 
-        $post = http_build_query(array('RequestType' => $requestID, 'ucid' =>
-        $username, 'password' => $password));
+        $tData = array('ucid' => $username, 'password' => $password);
 
-        $chBack = curl_init();
-        curl_setopt($chBack, CURLOPT_URL, $backurl);
-        curl_setopt($chBack, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($chBack, CURLOPT_POSTFIELDS, $post);
-
-        $resultB = curl_exec($chBack);
-        curl_close($chBack);
+        $post = http_build_query(array('RequestType' => $requestID, 'data' =>
+        $tData));
 
         $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, $backurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
         $result = curl_exec($ch);
-
+        echo $result; //Echos login return from back to front
         curl_close($ch);
-
-        if(strpos($result, "Invalid UCID")==false){
-                $response = "NJITyes";
-        }
-        else{
-                $response = "NJITno";
-        }
-
-        $decoded_json = json_decode($resultB, true);
-        $decoded_json['respNJIT'] = $response;
-        $finalJSON = json_encode($decoded_json, JSON_PRETTY_PRINT);
-        echo $finalJSON;
 
 }
 
@@ -56,11 +33,11 @@ if ($requestID == 'CreateQuestion'){
         $questiontext = $data['questiontext'];
         $testcases = $data['testcases'];
 
-        $sendData = array('topic' => $topic, 'difficulty' => $difficulty,
+        $tData = array('topic' => $topic, 'difficulty' => $difficulty,
         'questiontext' => $questiontext, 'testcases' => $testcases);
 
-        $datas = http_build_query(array('RequestType' => $requestID, data =>
-        $sendData));
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' =>
+        $tData));
 
         $ch = curl_init();
 
@@ -77,14 +54,13 @@ if ($requestID == 'CreateQuestion'){
 if ($requestID == 'GetQuestions'){//Send the request data forward for the
 //back to retreive the question data from the database to then send to front
 //Data will be holding the request type for back to determine which to send
-        $sendData = http_build_query(array('RequestType' => $requestID, data =>
-        ''));
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' => ''));
 
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $backurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $sendData);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
 
         $result = curl_exec($ch);
         echo $result;
@@ -98,11 +74,11 @@ if ($requestID == 'createExam'){
         $questID = $data['questionsid'];
         $questPoint = $data['questPoint'];
 
-        $sendData = array('exaName' => $examName, 'questionsid' => $questID,
+        $tData = array('exaName' => $examName, 'questionsid' => $questID,
         'questPoint' => $questPoint);
 
-        $datas = http_build_query(array('RequestType' => $requestID, data =>
-        $sendData));
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' =>
+        $tData));
 
         $ch = curl_init();
 
@@ -118,14 +94,14 @@ if ($requestID == 'createExam'){
 
 if ($requestID == 'listExams'){
 //Data will be sending the list of exams created to the front
-        $sendData = http_build_query(array('RequestType' => $requestID, data =>
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' =>
         ''));
 
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $backurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $sendData);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
 
         $result = curl_exec($ch);
         echo $result;
@@ -137,10 +113,10 @@ if($requestID == 'showExam'){
 //Data will be sending the exam chosen to the front to display
         $examName = $data['exaName'];
 
-        $sendData = array('exaName' => $examName);
+        $tData = array('exaName' => $examName);
 
-        $datas = http_build_query(array('RequestType' => $requestID, data =>
-        $sendData));
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' =>
+        $tData));
 
         $ch = curl_init();
 
@@ -164,15 +140,15 @@ if($requestID == 'showGradedExam'){
 
         $examName = $data['exaName'];
 
-        $dataz = array('exaName' => $examName);
+        $tData = array('exaName' => $examName);
 
-        $datas = http_build_query(array('RequestType' => $requestID, 'data' => $dataz));
+        $datas = http_build_query(array('RequestType' => $requestID, 'data' => $tData));
 
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $backurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $sendData);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
 
         $result = curl_exec($ch);
         echo $result;
@@ -265,5 +241,5 @@ if($requestID == 'retrieve'){
         curl_close($ch);
 
 }
-        
+
 ?>
