e0665529e00d8a9cab48389f3c9c179853387147
yvillalba <60801586+yvillalba@users.noreply.github.com>
2020-03-01 13:38:47 -0500

    Update testBackBetha.php

--- beta/testBackBetha.php
+++ beta/testBackBetha.php
@@ -9,7 +9,16 @@
 
 //$dat=array('RequestType'=>"listExams",'data'=>"");//data does not store anything
 
-$dat=array('RequestType'=>"showExam",'data'=>array('exaName'=>"exam345"));
+//$dat=array('RequestType'=>"showExam",'data'=>array('exaName'=>"exam345"));
+
+$dat=array('RequestType'=>"gradingExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'questionsid'=>array(31,39),
+         'answers'=>array("def square(start, end):
+    while start <= end:
+        start*=start
+    return start","stringNultiply(str1, num)
+	print(str1 * 2)"),'scores'=>array(50,26),'maxScores'=>array(50,50),'comments'=>array("coment1","coment2"),'expectedAnswers'=>array("256,81,16,16","foofoo, barbarbarbar, hihihi, worldworldworldworldworld"),'resultingAnswers'=>array("256,81,16,16","foofoo, barbar, hihi, worldworld"),'deductedPointsPerEachTest'=>array("0, 0, 0, 0","0, 5, 5, 5"),'deductedPointscorrectName'=>array(0,3),'deductedPointshasDef'=>array(0,3),'deductedPointsisMissingColon'=>array(0,3),'deductedPointsusedLoop'=>array(0,0)));
+
+
 
 $url="https://web.njit.edu/~yav3/backEndCS490Betha.php";
 
