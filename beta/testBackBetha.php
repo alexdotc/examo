f71502b46bce3411d5d64b4b3ff46dd40121c878
yvillalba <60801586+yvillalba@users.noreply.github.com>
2020-03-04 20:58:40 -0500

    Update testBackBetha.php

--- beta/testBackBetha.php
+++ beta/testBackBetha.php
@@ -9,7 +9,6 @@
 
 //$dat=array('RequestType'=>"listExams",'data'=>"");//data does not store anything
 
-
 //$dat=array('RequestType'=>"showExam",'data'=>array('exaName'=>"exam345"));
 //$dat=array('RequestType'=>"gradingExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'questionsid'=>array(31,39),
 //         'answers'=>array("def square(start, end):
@@ -18,12 +17,11 @@
 //    return start","stringNultiply(str1, num)
 //	print(str1 * 2)"),'scores'=>array(50,26),'maxScores'=>array(50,50),'comments'=>array("coment1","coment2"),'expectedAnswers'=>array("256,81,16,16","foofoo, barbarbarbar, hihihi, worldworldworldworldworld"),'resultingAnswers'=>array("256,81,16,16","foofoo, barbar, hihi, worldworld"),'deductedPointsPerEachTest'=>array("0, 0, 0, 0","0, 5, 5, 5"),'deductedPointscorrectName'=>array(0,3)));
 
-//$dat=array('RequestType'=>"showGradedExam",'data'=>array('exaName'=>"exam100"));
+$dat=array('RequestType'=>"showGradedExam",'data'=>array('exaName'=>"exam100",'ucid'=>'yav3'));
 //$dat=array('RequestType'=>"modifyGradedExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'gradesID'=>array(417,418),'answers'=>array("ans1","ans2"),'scores'=>array(10,20),'comments'=>array("comentnew1","coment2"),'released'=>"Y"));
 
 //$dat=array('RequestType'=>"listGradedExams",'data'=>"");//data does not store anything
-$dat=array('RequestType'=>"listGradedExamsStudent",'data'=>array('ucid'=>'yav3'));
-
+//$dat=array('RequestType'=>"listGradedExamsStudent",'data'=>array('ucid'=>'yav3'));//data does not store anything
 
 $url="https://web.njit.edu/~yav3/backEndCS490Betha.php";
 
