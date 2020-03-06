5bcebad282901343e5a605c7382bdb8fbc626478
AChavkin <alexlchavkin@gmail.com>
2020-03-05 19:23:15 -0500

    front: forgot to add files in last

---
+++ beta/front/studentView.php
@@ -0,0 +1,9 @@
+<?php
+	define('MAGICNUMBER', true);
+	include 'restrict.php';
+?>
+<div id="ViewExam" class="ViewItems ViewMain">
+	<h2 id="examName" class="ViewHeader"></h2>
+	<div id="ViewDiv" class="ViewDiv ViewItems">
+	</div>
+</div>
