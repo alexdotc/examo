866d68018ee07f08d2cf07be7defea6126fe59d2
yvillalba <60801586+yvillalba@users.noreply.github.com>
2020-03-12 14:21:02 -0400

    Update testBackBetha.php

--- beta/testBackBetha.php
+++ beta/testBackBetha.php
@@ -7,7 +7,7 @@
 //$dat=array('RequestType'=>"createExam",'data'=>array('exaName'=>"exam345",'questionsid'=>array(1,5),'questPoint'=>array(10,40)));
 
 
-$dat=array('RequestType'=>"listExams",'data'=>array('ucid'=>'yav3'));//
+//$dat=array('RequestType'=>"listExams",'data'=>array('ucid'=>'np595'));//data does not store anything
 
 //$dat=array('RequestType'=>"showExam",'data'=>array('exaName'=>"exam345"));
 //$dat=array('RequestType'=>"gradingExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'questionsid'=>array(31,39),
@@ -18,7 +18,7 @@ $dat=array('RequestType'=>"listExams",'data'=>array('ucid'=>'yav3'));//
 //	print(str1 * 2)"),'scores'=>array(50,26),'maxScores'=>array(50,50),'comments'=>array("coment1","coment2"),'expectedAnswers'=>array("256,81,16,16","foofoo, barbarbarbar, hihihi, worldworldworldworldworld"),'resultingAnswers'=>array("256,81,16,16","foofoo, barbar, hihi, worldworld"),'deductedPointsPerEachTest'=>array("0, 0, 0, 0","0, 5, 5, 5"),'deductedPointscorrectName'=>array(0,3)));
 
 //$dat=array('RequestType'=>"showGradedExam",'data'=>array('exaName'=>"exam100",'ucid'=>'yav3'));
-//$dat=array('RequestType'=>"modifyGradedExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'gradesID'=>array(417,418),'answers'=>array("ans1","ans2"),'scores'=>array(10,20),'comments'=>array("comentnew1","coment2"),'released'=>"Y"));
+$dat=array('RequestType'=>"modifyGradedExam",'data'=>array('ucid'=>'yav3','exaName'=>"exam100",'gradesID'=>array(417,418),'answers'=>array("ans1","ans2"),'scores'=>array(10,20),'comments'=>array("comentnew1","coment2"),'released'=>"Y",'deductedPointscorrectName'=>array(3,2)));
 
 //$dat=array('RequestType'=>"listGradedExams",'data'=>"");//data does not store anything
 //$dat=array('RequestType'=>"listGradedExamsStudent",'data'=>array('ucid'=>'yav3'));//data does not store anything
